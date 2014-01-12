e997f74705d9c28ede75084de25a44fe677bbd15
Oliver Switzer <oliverswitzer@gmail.com>
2014-01-11 20:16:52 -0500

    add views for song resource. Add controller actions show, new, create, index for song.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,32 @@
 class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(params[:song])
+    if @song.save
+      redirect_to @song
+    else
+      render "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
